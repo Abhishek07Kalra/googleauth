452b216fa6f463e740a5fe49ee0c3bdd1f2bde2d
Abhishek kalra <55233688+Abhishek07Kalra@users.noreply.github.com>
2021-03-27 13:09:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBVhzqkm7y8jNCZyGeAhB_KWr7f4Kuhhc",
-    authDomain: "loginsignup-3cc57.firebaseapp.com",
-    projectId: "loginsignup-3cc57",
-    storageBucket: "loginsignup-3cc57.appspot.com",
-    messagingSenderId: "918604516812",
-    appId: "1:918604516812:web:c388461ec3518f590a6169"
+    // your credentials :)
   };
 
 firebase.initializeApp(firebaseConfig);
